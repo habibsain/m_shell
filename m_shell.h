24c987b957574398eb6605963c736d03d33ab6d2
habibsain <habibsain25@gmail.com>
2025-06-19 12:30:59 +0530

    added new header file

---
+++ m_shell.h
@@ -0,0 +1,16 @@
+#ifndef __M_SHELL__
+#define __M_SHELL__
+
+#include<stdio.h>
+#include<stdlib.h>
+
+void scanner(int varc, char** varn);
+
+char** parser(char** varn);
+
+void command(char** tokens);
+
+
+
+
+#endif
